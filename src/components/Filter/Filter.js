05fb14e7c2c7133f379617806fd46e3129258a97
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-07-19 20:35:02 +0300

    добавляет localStorage

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './filter.module.css'
+
+const Filter =({ value, onChange }) => (
+    <label className={styles.filterLabel}>
+        Find contacts by name 
+    <input className={styles.filterInput} type="text"
+     value={value}
+     onChange={onChange}
+     /></label>
+);
+Filter.propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+  };
+export default Filter;
